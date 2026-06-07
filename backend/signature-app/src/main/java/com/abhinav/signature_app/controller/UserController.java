b5423b9dcdcde38965008c4f936973a7fab1d1ec
Abhinav Saxena <abhinavsaxena959@gmail.com>
2026-06-07 17:00:59 +0530

    Day 3: Implemented GET API with filtering using query params and streams

--- backend/signature-app/src/main/java/com/abhinav/signature_app/controller/UserController.java
+++ backend/signature-app/src/main/java/com/abhinav/signature_app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.abhinav.signature_app.controller;
 import com.abhinav.signature_app.model.User;
 import com.abhinav.signature_app.service.UserService;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -16,5 +17,12 @@ public class UserController {
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+    @GetMapping("/users")
+    public List<User> getAllUsers(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email
+    ) {
+        return userService.getAllUsers(name, email);
     }
 }
--- backend/signature-app/src/main/java/com/abhinav/signature_app/service/UserService.java
+++ backend/signature-app/src/main/java/com/abhinav/signature_app/service/UserService.java
@@ -3,6 +3,8 @@ import com.abhinav.signature_app.repository.UserRepository;
 import com.abhinav.signature_app.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -12,5 +14,12 @@ public class UserService {
     }
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+    public List<User> getAllUsers(String name, String email) {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .filter(user->name==null || user.getName().equalsIgnoreCase(name))
+                .filter(user->email==null || user.getEmail().equalsIgnoreCase(email))
+                .toList();
     }
 }
